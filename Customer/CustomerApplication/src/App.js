a291d810c26221061c1b9b06c82e8e9b0abc7537
Dhulasi Dheepthi D <85208116+dheepthi-d@users.noreply.github.com>
2023-04-28 10:06:05 +0530

    Update App.js

--- Customer/CustomerApplication/src/App.js
+++ Customer/CustomerApplication/src/App.js
@@ -4,6 +4,8 @@ import { Start } from "./pages/start/start";
 import { Shop } from "./pages/shop/shop";
 import { Home } from "./pages/home/home";
 import { Cart } from "./pages/cart/cart";
+import { MyOrders } from "./pages/myorders/myOrders";
+import { Checkout } from "./pages/cart/checkout";
 import { ShopContextProvider } from "./context/shop-context";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -17,10 +19,11 @@ function App() {
             <Route path="/" element={<Start />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Start />} />
             <Route path="/home" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/myorders" element={<MyOrders />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
         </Router>
       </ShopContextProvider>
