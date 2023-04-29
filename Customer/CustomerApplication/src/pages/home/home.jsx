2246c8ed328da08e93bc01f5fe7103da606f1c1f
Dhulasi Dheepthi D <85208116+dheepthi-d@users.noreply.github.com>
2023-04-29 20:19:54 +0530

    Update home.jsx

--- Customer/CustomerApplication/src/pages/home/home.jsx
+++ Customer/CustomerApplication/src/pages/home/home.jsx
@@ -1,35 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./home.css";
 import { Navbar } from "../../components/navbar";
-import axios from "axios";
-// import jwt from 'jsonwebtoken';
-import { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+
 export const Home = () => {
-  const navigate = useNavigate()
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('token')
-  //   if (token) {
-  //     const user = jwt.decode(token)
-  //     if (!user) {
-  //       localStorage.removeItem('token')
-  //       navigate('/login')
-  //     } else {
-
-  //     }
-  //   }
-  // },[])
-
   return (
     <div className="home">
       <Navbar />
       <div className="homeTitle">
-        <h1>Welcome to Pharma Blocks</h1>
-        {/* <a href="http://localhost:3500/getMeds"> Get Data</a> */}
-        
-
-
+        <h1>
+          Welcome to <span class="fancy">PharmaBlocks</span>
+        </h1>
       </div>
     </div>
   );
